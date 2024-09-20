af844d72108bbfcd86096e1394748b62747ced44
Tobias Kragholm <50784887+tkragholm@users.noreply.github.com>
2024-09-20 13:19:11 +0200

    Rename crate to "mary_elizabeth_utils" and add CLI scripts
    Title: Rename crate and add CLI scripts
    
    Summary:
    - Renamed the crate name in Cargo.toml from "_mary_elizabeth_utils" to "mary_elizabeth_utils"
    - Added two new CLI scripts to the pyproject.toml file:
      - "get_42" which calls the "get_42" function from the "pyo3_mixed" module
      - "print_cli_args" which calls the "print_cli_args" function from the "pyo3_mixed" module
    - Removed the unused build dependencies from Cargo.toml
    
    Rationale:
    The crate name was changed to better reflect the purpose of the library. The new CLI scripts provide a convenient way for users to interact with the library's functionality directly from the command line.

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,32 @@
-// use std::path::PathBuf;
+use pyo3::prelude::*;
+use std::env;
 
-// #[pyfunction]
-// fn get_readstat_path() -> PyResult<PathBuf> {
-//     let path = PathBuf::from(env!("READSTAT_BINARY"));
-//     if path.exists() {
-//         Ok(path)
-//     } else {
-//         Err(PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(
-//             "ReadStat binary not found at the expected location",
-//         ))
-//     }
-// }
+#[pyfunction]
+fn get_21() -> usize {
+    21
+}
 
-// /// A Python module implemented in Rust.
-// #[pymodule]
-// fn _mary_elizabeth_utils(m: &Bound<'_, PyModule>) -> PyResult<()> {
-//     m.add_function(wrap_pyfunction!(get_readstat_path, m)?)?;
-//     Ok(())
-// }
+/// Prints the CLI arguments, once from Rust's point of view and once from Python's point of view.
+#[pyfunction]
+fn print_cli_args(py: Python) -> PyResult<()> {
+    // This one includes Python and the name of the wrapper script itself, e.g.
+    // `["/home/ferris/.venv/bin/python", "/home/ferris/.venv/bin/print_cli_args", "a", "b", "c"]`
+    println!("{:?}", env::args().collect::<Vec<_>>());
+    // This one includes only the name of the wrapper script itself, e.g.
+    // `["/home/ferris/.venv/bin/print_cli_args", "a", "b", "c"])`
+    println!(
+        "{:?}",
+        py.import_bound("sys")?
+            .getattr("argv")?
+            .extract::<Vec<String>>()?
+    );
+    Ok(())
+}
+
+#[pymodule]
+fn mary_elizabeth_utils(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_wrapped(wrap_pyfunction!(get_21))?;
+    m.add_wrapped(wrap_pyfunction!(print_cli_args))?;
+
+    Ok(())
+}
